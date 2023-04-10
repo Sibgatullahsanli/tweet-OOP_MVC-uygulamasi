c257a3351edaa28f3bc9d8803a061a11641455bd
Sıbgatullah <sibgat.sanli@gmail.com>
2023-04-11 02:28:55 +0300

    tweter 2 odev eklendi

---
+++ Core/router.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace mEsin\Core;
+
+class Router
+{
+    private array $routes = [];
+    private string $controller_path;
+    private string $prefix;
+
+    public function __construct($params = [
+        'controller' => 'Controller',
+        'prefix' => ''
+    ])
+    {
+        $this->controller_path = $params['controller'];
+        $this->prefix = $params['prefix'];
+    }
+
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function post($path, $callback)
+    {
+        $this->routes['post'][$path] = $callback;
+    }
+
+    public function put($path, $callback)
+    {
+        $this->routes['put'][$path] = $callback;
+    }
+
+    public function delete($path, $callback)
+    {
+        $this->routes['delete'][$path] = $callback;
+    }
+
+
+    public function run()
+    {
+        $find = false;
+
+        $req_method = strtolower($_SERVER['REQUEST_METHOD']);
+        $basedir = dirname($_SERVER['SCRIPT_NAME']);
+        $uri = $_SERVER['REQUEST_URI'];
+        $request = str_replace($basedir, '', $uri);
+        // $request = preg_replace('#/[0-9]+$#', '', $request);
+        // exit($request);
+        foreach ($this->routes[$req_method] as $path => $callback) {
+            if (preg_match("#^{$path}$#", $request, $mathes)) {
+                unset($mathes[0]);
+                $params = $mathes;
+
+                $find = true;
+                if (is_callable($callback)) {
+                    call_user_func_array($callback, $params);
+                    break;
+                } else {
+
+                    [$controller, $method] = explode('@', $callback);
+                    $controller_file_name =  $this->controller_path . DIRECTORY_SEPARATOR . $controller . '.php';
+                    $controller_name = $this->prefix . '\\' . $this->controller_path . '\\' . $controller;
+
+                    if (file_exists($controller_file_name)) {
+                        include_once($controller_file_name);
+                    } else {
+                        $find = false;
+                    }
+
+                    if (method_exists($controller_name, $method)) {
+                        call_user_func([new $controller_name, $method], $params);
+                        break;
+                    } else {
+                        $find = false;
+                    }
+                }
+            }
+        }
+        if (!$find) {
+            // echo '<h1>Sayfa bulunamadı!</h1>';
+            $controller_file_name =  $this->controller_path . DIRECTORY_SEPARATOR . '_404' . '.php';
+            $controller_name = $this->prefix . '\\' . $this->controller_path . '\\' . '_404';
+            include_once($controller_file_name);
+            call_user_func([new $controller_name, 'index']);
+        }
+    }
+}
